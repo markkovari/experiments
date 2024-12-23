0267deebe2574af04ff0fbde88c2c50c68cfee4c
Márk Kővári <kovarimarkofficial@gmail.com>
2024-12-23 11:02:32 +0100

    chore(folder): create folder structure for reusable packages
    Signed-off-by: Márk Kővári <kovarimarkofficial@gmail.com>

---
+++ callsayfibo/cmd/cli.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"markkovari/callsayfibo/pgk/fibonacci"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	fibo := fibonacci.New()
+	current := int64(0)
+	for {
+		cmd := exec.Command("say", fmt.Sprintf("%d", fibo.Get(current)))
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Duration(time.Millisecond * 100))
+		current += 1
+	}
+}
