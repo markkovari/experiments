a6e902ff379728be5d4e8000905e78ca5677f905
Márk Kővári <kovarimarkofficial@gmail.com>
2025-01-22 21:56:19 +0100

    chore(): gh action fix with docker
    Signed-off-by: Márk Kővári <kovarimarkofficial@gmail.com>

--- .github/actions/deleteIf/index.js
+++ .github/actions/deleteIf/index.js
@@ -1,15 +1,15 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, setOutput, setFailed } from '@actions/core';
+import { context } from '@actions/github';
 
 try {
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}`);
-
-    const time = new Date().toTimeString();
-    core.setOutput('time', time); // Set output for later use
-
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 } catch (error) {
-    core.setFailed(error.message);
+    setFailed(error.message);
 }
