e66f4dcdb42136382bb818942541cc7e9dfa9f7a
markkovari <kovarimarkofficial@gmail.com>
2025-02-16 12:59:59 +0100

    chore(): migrated to its own repo
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- petClinic/src/routes/plugin@auth.ts
+++
@@ -1,8 +0,0 @@
-import { QwikAuth$ } from "@auth/qwik";
-import Discord from "@auth/qwik/providers/discord";
-
-export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
-  () => ({
-    providers: [Discord],
-  }),
-);
