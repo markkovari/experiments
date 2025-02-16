a01e1a34b0ad6c5e615fd5e840b6a4b4105621da
markkovari <kovarimarkofficial@gmail.com>
2025-02-16 12:29:03 +0100

    chore(user): add discrod sign-in
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- petClinic/src/routes/layout.tsx
+++ petClinic/src/routes/layout.tsx
@@ -2,10 +2,9 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "../components/starter/header/header";
-import Footer from "../components/starter/footer/footer";
-
 import styles from "./styles.css?inline";
+import { Header } from "~/components/header/Header";
+import { Footer } from "~/components/footer/Footer";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
