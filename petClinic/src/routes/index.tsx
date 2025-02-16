39e4bd8ec474ad3298c7a1576d141ec244737e84
markkovari <kovarimarkofficial@gmail.com>
2025-02-16 12:39:07 +0100

    chore(ci): setup conventional things
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- petClinic/src/routes/index.tsx
+++ petClinic/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
-import { Form, type DocumentHead } from "@builder.io/qwik-city";
+import { type DocumentHead, Form } from "@builder.io/qwik-city";
+import { SignIn } from "~/components/sign-in";
 import { useSession, useSignIn, useSignOut } from "./plugin@auth";
 
 export default component$(() => {
@@ -9,26 +10,16 @@ export default component$(() => {
 
   return (
     <>
-      <div role="presentation" class="ellipsis"></div>
       {user ? (
         <>
           <h1>Welcome, {user.user?.name}!</h1>
           <Form action={signOut}>
-            <button>Sign Out</button>
+            <button type="button">Sign Out</button>
           </Form>
         </>
       ) : (
-        <Form action={signIn}>
-          <input type="hidden" name="providerId" value="github" />
-          <input
-            type="hidden"
-            name="options.redirectTo"
-            value="http://localhost:3000/auth/discord/callback"
-          />
-          <button>Sign In</button>
-        </Form>
+        <SignIn />
       )}
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
     </>
   );
 });
