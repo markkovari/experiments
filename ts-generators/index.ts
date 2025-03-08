64044516793d80b8ddbb7e5bfa8db30d8cb7f1b9
markkovari <kovarimarkofficial@gmail.com>
2025-03-09 00:20:53 +0100

    chore(): add generators
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

---
+++ ts-generators/index.ts
@@ -0,0 +1,37 @@
+function* calculateFibonacci(): Generator<number> {
+    let prev = 0;
+    let curr = 1;
+
+    yield prev;
+    yield curr;
+
+    while (true) {
+        const next = prev + curr;
+        yield next;
+        prev = curr;
+        curr = next;
+    }
+}
+
+function* generateUntil(until: number): Generator<number> {
+    let curr = 1;
+    yield curr;
+    while (curr !== until) {
+        curr++;
+        yield curr;
+    }
+}
+
+// const fibonacciGenerator = calculateFibonacci();
+
+// // Calculate the first 10 Fibonacci numbers
+// for (let i = 0; i < 1000; i++) {
+//     console.log(fibonacciGenerator.next().value);
+//     // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+// }
+
+const generateUntilGenerator = generateUntil(100);
+
+for (const curr of generateUntilGenerator) {
+    console.log({ curr })
+}
