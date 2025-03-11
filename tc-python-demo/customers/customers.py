06c0551a629320bbeb0ee5bbe8d4c377382d5407
markkovari <kovarimarkofficial@gmail.com>
2025-03-11 21:00:42 +0100

    chore(bdd): add behave test framework with add tests
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- tc-python-demo/customers/customers.py
+++ tc-python-demo/customers/customers.py
@@ -1,26 +1,32 @@
 import os
 import psycopg2
 
+
 def get_connection():
+    print("Trying to connect to: ", os.getenv("DB_NAME"))
     return psycopg2.connect(
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USERNAME"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        port=os.getenv("DB_PORT"),
     )
+
 
 def create_table():
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("""
+            cur.execute(
+                """
                 CREATE TABLE IF NOT EXISTS customers (
                     id SERIAL PRIMARY KEY,
                     name VARCHAR(255) NOT NULL,
                     email VARCHAR(255) UNIQUE NOT NULL
                 );
-            """)
+            """
+            )
             conn.commit()
+
 
 def drop_table():
     with get_connection() as conn:
@@ -28,11 +34,15 @@ def drop_table():
             cur.execute("DROP TABLE customers;")
             conn.commit()
 
+
 def create_customer(name, email):
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute("INSERT INTO customers (name, email) VALUES (%s, %s)", (name, email))
+            cur.execute(
+                "INSERT INTO customers (name, email) VALUES (%s, %s)", (name, email)
+            )
             conn.commit()
+
 
 def get_all_customers():
     with get_connection() as conn:
@@ -40,11 +50,13 @@ def get_all_customers():
             cur.execute("SELECT * FROM customers")
             return cur.fetchall()
 
+
 def get_customer_by_email(email):
     with get_connection() as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM customers WHERE email = %s", (email,))
             return cur.fetchone()
+
 
 def delete_all_customers():
     with get_connection() as conn:
---
+++ tc-python-demo/features/environment.py
@@ -0,0 +1,35 @@
+import os
+import logging
+from behave.fixture import fixture, use_fixture_by_tag
+from testcontainers.postgres import PostgresContainer
+
+
+@fixture
+def postgres_container(context):
+    logging.getLogger("testcontainers").setLevel(logging.CRITICAL)
+    context.postgres = PostgresContainer("postgres:16-alpine")
+    context.postgres.start()
+
+    os.environ["DB_HOST"] = context.postgres.get_container_host_ip()
+    os.environ["DB_PORT"] = context.postgres.get_exposed_port(5432)
+    os.environ["DB_USERNAME"] = context.postgres.username
+    os.environ["DB_PASSWORD"] = context.postgres.password
+    os.environ["DB_NAME"] = context.postgres.dbname
+
+    yield context.postgres
+
+    context.postgres.stop()
+
+
+fixture_registry = {
+    "fixture.postgres": postgres_container,
+}
+
+
+def before_tag(context, tag):
+    if tag.startswith("fixture."):
+        return use_fixture_by_tag(tag, context, fixture_registry)
+
+
+def before_all(context):
+    logging.getLogger(name="testcontainers").setLevel(logging.CRITICAL)
