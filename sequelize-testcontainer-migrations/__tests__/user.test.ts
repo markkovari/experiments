2455670e94787a3fe6a676156d616aa24ee336f7
markkovari <kovarimarkofficial@gmail.com>
2025-03-21 14:05:08 +0100

    chore(sequelize): add tests with a postgres container
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

---
+++ sequelize-testcontainer-migrations/__tests__/user.test.ts
@@ -0,0 +1,67 @@
+import {
+  PostgreSqlContainer,
+  type StartedPostgreSqlContainer,
+} from "@testcontainers/postgresql";
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
+
+import type { Model, ModelCtor, Sequelize } from "sequelize";
+import { getSequelize } from "../src/database";
+
+let container: PostgreSqlContainer;
+let startedContainer: StartedPostgreSqlContainer;
+let sequelize: Sequelize;
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+let UserModel: ModelCtor<Model<any, any>>;
+
+beforeAll(async () => {
+  container = new PostgreSqlContainer("postgres:16-alpine").withExposedPorts(
+    5432,
+  );
+  startedContainer = await container.start();
+  const { client, User } = await getSequelize({
+    database: startedContainer.getDatabase(),
+    dialect: "postgres",
+    host: startedContainer.getHost(),
+    password: startedContainer.getPassword(),
+    port: startedContainer.getPort(),
+    username: startedContainer.getPassword(),
+  });
+  UserModel = User;
+  sequelize = client;
+});
+
+beforeEach(async () => {
+  // option one
+  // await UserModel.destroy({ force: true, cascade: true, truncate: true });
+  await sequelize.truncate({ force: true, truncate: true });
+});
+
+afterAll(async () => {
+  await sequelize.close();
+  await startedContainer.stop();
+});
+
+describe("database", () => {
+  describe("functionaly", () => {
+    it("should be able to store users", async () => {
+      const savedUser = await UserModel.create({
+        firstName: "Mark",
+        lastName: "Kovari",
+      });
+      expect(savedUser).not.toBeNull();
+    });
+
+    it("should be able to store multiple users", async () => {
+      const savedUser = await UserModel.create({
+        firstName: "Mark",
+        lastName: "Kovari",
+      });
+      const savedUser2 = await UserModel.create({
+        firstName: "Mark",
+        lastName: "Kovari",
+      });
+      const userAmount = await UserModel.count();
+      expect(userAmount).toBe(2);
+    });
+  });
+});
