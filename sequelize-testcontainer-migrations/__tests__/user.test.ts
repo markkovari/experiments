e8b15dbc8404010f20489364d0b57d28228ddafd
markkovari <kovarimarkofficial@gmail.com>
2025-03-21 19:08:59 +0100

    refactor(tests): create test context
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- sequelize-testcontainer-migrations/__tests__/user.test.ts
+++ sequelize-testcontainer-migrations/__tests__/user.test.ts
@@ -1,56 +1,11 @@
-import {
-  PostgreSqlContainer,
-  type StartedPostgreSqlContainer,
-} from "@testcontainers/postgresql";
-import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
-
-import type { Model, ModelCtor, Sequelize } from "sequelize";
-import { getSequelize } from "../src/database";
-
-let container: PostgreSqlContainer;
-let startedContainer: StartedPostgreSqlContainer;
-let sequelize: Sequelize;
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-let UserModel: ModelCtor<Model<any, any>>;
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-let PaymentModel: ModelCtor<Model<any, any>>;
-
-beforeAll(
-  async () => {
-    container = new PostgreSqlContainer("postgres:16-alpine").withExposedPorts(
-      5432,
-    );
-    startedContainer = await container.start();
-    const { client, User, Payment } = await getSequelize({
-      database: startedContainer.getDatabase(),
-      dialect: "postgres",
-      host: startedContainer.getHost(),
-      password: startedContainer.getPassword(),
-      port: startedContainer.getPort(),
-      username: startedContainer.getPassword(),
-    });
-    UserModel = User;
-    PaymentModel = Payment;
-    sequelize = client;
-  },
-  3 * 60 * 1000,
-); // testcontainers take some time to load initially - 3 minutes is given
-
-beforeEach(async () => {
-  // option one
-  // await UserModel.destroy({ force: true, cascade: true, truncate: true });
-  await sequelize.truncate({ force: true, truncate: true, cascade: true });
-});
-
-afterAll(async () => {
-  await sequelize.close();
-  await startedContainer.stop();
-});
+import { describe, expect, it } from "vitest";
+import { Payment } from "../src/infrastructure/database/models/Payment";
+import { User } from "../src/infrastructure/database/models/User";
 
 describe("database", () => {
   describe("functionaly", () => {
     it("should be able to store users", async () => {
-      const savedUser = await UserModel.create({
+      const savedUser = await User.create({
         firstName: "Mark",
         lastName: "Kovari",
       });
@@ -58,29 +13,29 @@ describe("database", () => {
     });
 
     it("should be able to store multiple users", async () => {
-      const savedUser = await UserModel.create({
+      const savedUser = await User.create({
         firstName: "Mark",
         lastName: "Kovari",
       });
-      const savedUser2 = await UserModel.create({
+      const savedUser2 = await User.create({
         firstName: "Mark",
         lastName: "Kovari",
       });
-      const userAmount = await UserModel.count();
+      const userAmount = await User.count();
       expect(userAmount).toBe(2);
     });
 
     it("should be able to create transfers from one account to another", async () => {
-      const oldG = await UserModel.create({
+      const oldG = await User.create({
         firstName: "George",
         lastName: "Soros",
       });
-      const me = await UserModel.create({
+      const me = await User.create({
         firstName: "Mark",
         lastName: "Kovari",
       });
 
-      const payment = await PaymentModel.create({
+      const payment = await Payment.create({
         amount: 10000000,
         //@ts-ignore
         from: oldG.dataValues.id,
---
+++ sequelize-testcontainer-migrations/setupTests.ts
@@ -0,0 +1,45 @@
+import {
+  PostgreSqlContainer,
+  type StartedPostgreSqlContainer,
+} from "@testcontainers/postgresql";
+import type { Sequelize } from "sequelize";
+import { afterAll, beforeAll, beforeEach } from "vitest";
+import { getSequelize } from "./src/database";
+import { definePaymentModel } from "./src/infrastructure/database/models/Payment";
+import { defineUserModel } from "./src/infrastructure/database/models/User";
+
+let container: PostgreSqlContainer;
+let startedContainer: StartedPostgreSqlContainer;
+let sequelize: Sequelize;
+
+beforeAll(
+  async () => {
+    container = new PostgreSqlContainer("postgres:16-alpine").withExposedPorts(
+      5432,
+    );
+    startedContainer = await container.start();
+    const { client, User, Payment } = await getSequelize({
+      database: startedContainer.getDatabase(),
+      dialect: "postgres",
+      host: startedContainer.getHost(),
+      password: startedContainer.getPassword(),
+      port: startedContainer.getPort(),
+      username: startedContainer.getPassword(),
+    });
+    defineUserModel(client);
+    definePaymentModel(client);
+    sequelize = client;
+  },
+  3 * 60 * 1000,
+);
+
+beforeEach(async () => {
+  await sequelize.truncate({ force: true, truncate: true, cascade: true });
+});
+
+afterAll(async () => {
+  await sequelize.close();
+  await startedContainer.stop();
+});
+
+export { container, startedContainer, sequelize };
