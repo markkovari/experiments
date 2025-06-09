f7f38d37cfcb95f7eaf52231af3a765a227b8ed5
markkovari <kovarimarkofficial@gmail.com>
2025-06-09 15:36:15 +0200

    chore: change runner tp pm2

--- sigTermGrace/index.ts
+++ sigTermGrace/index.ts
@@ -1,22 +1,33 @@
-import express, { type Request, type Response } from 'express'
+import express, { type Request, type Response } from "express";
 
-import { withConnectionTracking } from "./withGracefulShutdown"
+import { withConnectionTracking } from "./withGracefulShutdown";
 
 const isGracefulEnabled = process.env.FEATURE_GRACEFUL_ENABLED === "true";
+const port = process.env.PORT;
+
+if (port === undefined && !Number.isInteger(port)) {
+	console.error(`PORT must be a number ${port}`);
+	process.exit(1);
+}
+const portAsNum = Number.parseInt(port as string);
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world!');
-    return;
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello world!");
+	return;
 });
 
-const { shutdown, getActiveConnectionCount } = withConnectionTracking(app, 8000, isGracefulEnabled);
+const { shutdown, getActiveConnectionCount } = withConnectionTracking(
+	app,
+	portAsNum,
+	isGracefulEnabled,
+);
 
 if (isGracefulEnabled) {
-    process.on('SIGTERM', shutdown);
-    process.on('SIGINT', shutdown);
-    setInterval(() => {
-        console.log(`Open connections: ${getActiveConnectionCount()}`);
-    }, 100);
+	process.on("SIGTERM", shutdown);
+	process.on("SIGINT", shutdown);
+	setInterval(() => {
+		console.log(`Open connections: ${getActiveConnectionCount()}`);
+	}, 100);
 }
