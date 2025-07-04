2d19773dd68925c983ebb7dd85a9a82221617471
markkovari <kovarimarkofficial@gmail.com>
2025-07-05 00:46:07 +0200

    chore: add containers and func to vonnect WIP
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- rate-limit-queues/app/events/events.go
+++ rate-limit-queues/app/events/events.go
@@ -1,7 +1,9 @@
 package events
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
 	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -10,7 +12,15 @@ import (
 const QueueName = "some_thing_to_publish_to"
 
 func CreateConn() (*amqp.Connection, error) {
-	conn, err := amqp.Dial("amqp://user:password@localhost:5672/")
+	for i, e := range os.Environ() {
+		fmt.Println("> %d %v", i, e)
+	}
+	rabbitURL := os.Getenv("RABBITMQ_URL")
+	if rabbitURL == "" {
+		rabbitURL = "amqp://user:password@rabbitmq:5672/" // fallback default
+	}
+	fmt.Println("RabbitMQ URL:", rabbitURL)
+	conn, err := amqp.Dial(rabbitURL)
 
 	if err != nil {
 		slog.Error("cannot connect to rabbitmq")
@@ -21,10 +31,9 @@ func CreateConn() (*amqp.Connection, error) {
 	return conn, nil
 }
 
-func CreateQueue(queueName string) (*amqp.Queue, error) {
+func InitQueue(queueName string) (*amqp.Queue, error) {
 	conn, err := CreateConn()
 	if err != nil {
-
 		slog.Error("cannot open conn")
 		return nil, err
 	}
@@ -51,7 +60,7 @@ func CreateQueue(queueName string) (*amqp.Queue, error) {
 
 }
 
-func SendMessageChannel(channel amqp.Channel, message []byte) error {
+func SendMessageChannel(channel *amqp.Channel, message []byte) error {
 	err := channel.Publish(
 		"",
 		QueueName,
@@ -62,5 +71,34 @@ func SendMessageChannel(channel amqp.Channel, message []byte) error {
 			Body:        message,
 			Timestamp:   time.Now(),
 		})
+	return err
+}
+
+type MessageHandler func(amqp.Delivery)
+
+func ConsumeMessages(ch *amqp.Channel, handler MessageHandler) error {
+
+	msgs, err := ch.Consume(
+		QueueName,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+	forever := make(chan bool)
+	go func() {
+		for msg := range msgs {
+			handler(msg)
+		}
+	}()
+
+	<-forever
+
 	return err
 }
---
+++ rate-limit-queues/app/worker.Dockerfile
@@ -0,0 +1,45 @@
+FROM golang:1.24-alpine AS builder
+
+# Set the current working directory inside the container
+WORKDIR /app
+
+# Copy go mod and sum files to leverage Docker layer caching
+COPY go.mod go.sum ./
+
+# Download all dependencies. This step is cached if go.mod and go.sum don't change.
+RUN go mod download
+
+# Copy the rest of the application source code
+COPY . .
+
+# Build the Go application
+# CGO_ENABLED=0 is crucial for static compilation, making the binary self-contained.
+# GOOS=linux ensures the binary is built for Linux (the base image).
+# -a -installsuffix cgo helps with static linking.
+# -o main specifies the output executable name.
+# -ldflags "-s -w" reduces the binary size by removing debug information.
+
+RUN CGO_ENABLED=0 GOOS=linux go build ./cmd/worker/main.go
+
+# Use a minimal Alpine image for the final stage to create a small production image
+FROM alpine:latest
+
+# setup app user
+RUN addgroup -g 1001 appuser && adduser -u 1001 -D -G appuser appuser
+
+# Set the current working directory in the final image
+WORKDIR /root/
+
+# Copy the compiled application binary from the builder stage
+COPY --from=builder --chown=appuser:appuser /app/main .
+
+# make main runnable
+RUN chmod +x main
+
+# Set the user for subsequent commands and the CMD instruction
+USER appuser
+# Expose the port the application will listen on
+EXPOSE 8080
+
+# Command to run the application when the container starts
+CMD ["./main"]
