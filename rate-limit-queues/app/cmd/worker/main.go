66013b7967da3c654e00986029ea00d76648b939
markkovari <kovarimarkofficial@gmail.com>
2025-07-05 12:18:38 +0200

    feat: add event sub

--- rate-limit-queues/app/cmd/worker/main.go
+++ rate-limit-queues/app/cmd/worker/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "log/slog"
+import (
+	"asdasd/events"
+	"log/slog"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+func handleMessage(msg amqp091.Delivery) {
+	slog.Info("event got", slog.Group("message details",
+		slog.String("body_preview", string(msg.Body)),
+	))
+
+}
 
 func main() {
 
 	slog.Info("worker starts")
+
+	if err := events.ConsumeMessagesFromChannel(handleMessage); err != nil {
+		slog.Error("Cannot create message consumer")
+	}
+
 }
