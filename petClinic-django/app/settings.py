0a15ad11311e134733bd5dee7dfde073ecaa7355
markkovari <kovarimarkofficial@gmail.com>
2025-08-10 00:32:40 +0200

    chore: add printenv tool and add postgres volume
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- petClinic-django/app/settings.py
+++ petClinic-django/app/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for app project.
 
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "mydb"),
+        "USER": os.getenv("POSTGRES_USER", "myuser"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "mypassword"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
