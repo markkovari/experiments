3044819a460ce8b1ad3634a27ac3590aad76aa19
Márk Kővári <kovarimarkofficial@gmail.com>
2025-08-22 02:11:25 +0200

    chore/sqlite turso (#116)
    * chore: add sqlite file sqlite version
    
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>
    
    * chore: add good gitiginore
    
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>
    
    * chore: add inmemory tests and ci
    
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>
    
    * fix: ci name
    
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>
    
    * chore: update golang ci lint version
    
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>
    
    ---------
    
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

---
+++ sqlite-backend/go.mod
@@ -0,0 +1,18 @@
+module sqlite-backend
+
+go 1.25.0
+
+require (
+	github.com/pressly/goose/v3 v3.24.3
+	github.com/tursodatabase/go-libsql v0.0.0-20250723062947-60e59c7150f4
+)
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+)
---
+++ sqlite-backend/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"math/rand/v2"
+	gen "sqlite-backend/db/gen"
+
+	_ "github.com/tursodatabase/go-libsql"
+)
+
+func main() {
+	db, err := sql.Open("libsql", "file:./local.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create a new sqlc queries object
+	queries := gen.New(db)
+	ctx := context.Background()
+
+	rand := rand.Uint32()
+
+	// Use the generated typesafe functions
+	userParams := gen.CreateUserParams{
+		Name:  "John Doe",
+		Email: fmt.Sprintf("john.doe%d@example.com", rand),
+	}
+	user, err := queries.CreateUser(ctx, userParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Created user:", user.Name)
+
+	// Retrieve the user by ID
+	retrievedUser, err := queries.GetUserByID(ctx, user.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Retrieved user:", retrievedUser.Email)
+}
