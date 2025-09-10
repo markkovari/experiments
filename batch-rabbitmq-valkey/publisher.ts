887e807d9d0dfe414cefce13d82562fbe1df895d
markkovari <kovarimarkofficial@gmail.com>
2025-09-10 18:34:01 +0200

    chore: add things, but should use valkey json instead
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- batch-rabbitmq-valkey/publisher.ts
+++ batch-rabbitmq-valkey/publisher.ts
@@ -1,29 +1,44 @@
-import {connect} from "amqplib";
-import {  exchangeName, topicName, getConf, type SomeMessage } from "./common.js";
+import { connect } from "amqplib";
+import {
+	exchangeName,
+	topicName,
+	getConf,
+	type SomeMessage,
+} from "./common.js";
 import { createBatch } from "./batch.js";
 
-const { mq: { url } } = getConf();
+const {
+	mq: { url },
+} = getConf();
 
 const run = async () => {
-    const connection = await connect(url)
-    const channel = await connection.createChannel();
-    await channel.assertExchange(exchangeName, "topic", { durable: true });
+	const connection = await connect(url);
+	const channel = await connection.createChannel();
+	await channel.assertExchange(exchangeName, "topic", { durable: true });
 
-    const randomElementLength = Math.floor(Math.random() * 1000);
-    const batchElements: SomeMessage[] = [...new Array(randomElementLength).keys()].map((i) => ({someValue: `asd${Math.floor(Math.random() * 1000)}`}));
-    const batch = await createBatch<SomeMessage>(batchElements);
-    for (const element of batch.elements) {
-        const didSend = channel.publish(exchangeName, topicName, Buffer.from(JSON.stringify(element)), {persistent: true});
-    }
+	// const randomElementLength = Math.floor(Math.random() * 4);
+	const randomElementLength = 1;
+	const batchElements: SomeMessage[] = [
+		...new Array(randomElementLength).keys(),
+	].map((i) => ({ someValue: `asd_${Math.floor(Math.random() * 1000)}` }));
+	const batch = await createBatch<SomeMessage>(batchElements);
+	for (const element of batch.elements) {
+		const didSend = channel.publish(
+			exchangeName,
+			topicName,
+			Buffer.from(JSON.stringify(element)),
+			{ persistent: true },
+		);
+	}
 
-    setTimeout(() => {
-        connection.close();
-        process.exit(0);
-    }, 500); 
-}
+	setTimeout(() => {
+		connection.close();
+		process.exit(0);
+	}, 500);
+};
 
 try {
-    await run()
-} catch(error) {
-    console.error("Cannot run publisher", {error})
+	await run();
+} catch (error) {
+	console.error("Cannot run publisher", { error });
 }
