355ff130d257a54a36eb50c35b5828f815c5bd61
markkovari <kovarimarkofficial@gmail.com>
2025-09-10 11:46:33 +0200

    chore: add valkey client
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- batch-rabbitmq-valkey/common.ts
+++ batch-rabbitmq-valkey/common.ts
@@ -1,3 +1,33 @@
 export const exchangeName = "anyExchangeName";
 export const queueName = "anyQueueName";
 export const topicName = "topicName";
+
+export type RabbitMqConfig = {
+	url: string;
+};
+
+export type ValKeyConf = {
+	host: string;
+	port: number;
+	username: string;
+	password: string;
+};
+
+export type Config = {
+	cache: ValKeyConf;
+	mq: RabbitMqConfig;
+};
+
+export const getConf = (): Config => {
+	return {
+		mq: {
+			url: "amqp://user:password@localhost",
+		},
+		cache: {
+			host: "localhost",
+			password: "",
+			port: 6379,
+			username: "",
+		},
+	};
+};
--- batch-rabbitmq-valkey/publisher.ts
+++ batch-rabbitmq-valkey/publisher.ts
@@ -1,17 +1,18 @@
 import {connect} from "amqplib";
-import {  exchangeName, queueName, topicName } from "./common.js";
+import {  exchangeName, topicName, getConf } from "./common.js";
+
+const { mq: { url } } = getConf()
 
 const run = async () => {
-    const connection = await connect("amqp://user:password@localhost")
+    const connection = await connect(url)
     const channel = await connection.createChannel();
     await channel.assertExchange(exchangeName, "topic", { durable: true });
-
 
     for(let i = 0; i < 2; i++) {
         const didSend = channel.publish(exchangeName, topicName, Buffer.from("SomeMessage"), {persistent: true});
     }
 
-    setTimeout(function () {
+    setTimeout(() => {
         connection.close();
         process.exit(0);
     }, 500); 
