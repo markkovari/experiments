663fa6b9c21051ac1c36aeae82cef31baa731924
markkovari <kovarimarkofficial@gmail.com>
2025-12-11 11:49:22 +0100

    chore: add prisma 67 migration POC
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

---
+++ prisma-67/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import type { Request, Response } from "express";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const app = express();
+
+const connectionString = process.env.DATABASE_URL || "postgresql://localhost:5432/mydb";
+
+const pool = new Pool({
+    connectionString,
+});
+
+const adapter = new PrismaPg(pool);
+
+const client = new PrismaClient({
+    adapter,
+});
+
+app.get("/", async (req: Request, res:Response) => {
+    const users = await client.user.findMany();
+    return res.json({ users });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
