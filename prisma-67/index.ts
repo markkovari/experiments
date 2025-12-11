950913a583a10bab6be687d578d9b2e7e6337130
markkovari <kovarimarkofficial@gmail.com>
2025-12-11 16:19:59 +0100

    chore: add setup service file
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- prisma-67/index.ts
+++ prisma-67/index.ts
@@ -3,28 +3,36 @@ import type { Request, Response } from "express";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
+import { randomUUID } from "node:crypto";
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-const connectionString = process.env.DATABASE_URL || "postgresql://localhost:5432/mydb";
+const connectionString =
+	process.env.DATABASE_URL || "postgresql://localhost:5432/mydb";
 
 const pool = new Pool({
-    connectionString,
+	connectionString
 });
 
 const adapter = new PrismaPg(pool);
 
 const client = new PrismaClient({
-    adapter,
+	adapter,
 });
 
-app.get("/", async (req: Request, res:Response) => {
-    const users = await client.user.findMany();
-    return res.json({ users });
+app.get("/add", async (req: Request, res: Response) => {
+    const newUser = await client.user.create({data :{ email: randomUUID() }});
+    return res.json({ newUser });
 });
+
+app.get("/", async (req: Request, res: Response) => {
+	const users = await client.user.findMany();
+	return res.json({ users });
+});
+
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
